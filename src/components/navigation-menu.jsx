ce27a31a4b854a87ba8e76f6bd1735f860f29110
Tret Elena <etret161@gmail.com>
2024-12-01 19:48:25 +0200

    changed the UI of the navigation menu

--- src/components/navigation-menu.jsx
+++ src/components/navigation-menu.jsx
@@ -1,183 +1,86 @@
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Container from "@mui/material/Container";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
-import {
-  Home as HomeIcon,
-  Search as SearchIcon,
-  Favorite as FavoriteIcon,
-} from "@mui/icons-material";
-import LoginIcon from "@mui/icons-material/Login";
-import {
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { useEffect } from "react";
 import { useAuthentication } from "@/utils/authenticationProvider";
+import styles from "@/styles/navigation-menu.module.css";
 
 const pages = [
-  { text: "Home", icon: <HomeIcon />, path: "/home" },
-  { text: "Search", icon: <SearchIcon />, path: "/search" },
-  { text: "Favorites", icon: <FavoriteIcon />, path: "/favorites" },
+  {
+    text: "Home",
+    icon: <span className="material-symbols-outlined">house</span>,
+    path: "/home",
+  },
+  {
+    text: "Search",
+    icon: <span className="material-symbols-outlined">location_searching</span>,
+    path: "/search",
+  },
+  {
+    text: "Favorites",
+    icon: <span className="material-symbols-outlined">favorite</span>,
+    path: "/favorites",
+  },
 ];
-
-const profileActions = ["Logout"];
 
 export default function NavigationMenu() {
   const router = useRouter();
-
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const { isUserAuthenticated, setIsUserAuthenticated } = useAuthentication();
 
   useEffect(() => {
     const user = localStorage.getItem("user");
-    if (user) {
-      setIsUserAuthenticated(true);
-    } else {
-      setIsUserAuthenticated(false);
-    }
-    console.log("user", user);
+    setIsUserAuthenticated(!!user);
   }, []);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const handleNavigate = (path) => {
+  const handleNavigation = (path) => {
     router.push(path);
-    handleCloseNavMenu();
   };
 
   const handleLogout = () => {
     localStorage.removeItem("user");
     setIsUserAuthenticated(false);
-    handleCloseUserMenu();
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem
-                  key={page.text}
-                  onClick={() => handleNavigate(page.path)}
-                  sx={{ display: "flex", alignItems: "center", gap: 1 }}
-                >
-                  {page.icon}
-                  <Typography textAlign="center">{page.text}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+    <>
+      <nav className={styles.navbar}>
+        <ul className={styles.navList}>
+          {pages.map(({ text, icon, path }) => (
+            <li key={text} className={styles.navItem}>
+              <button
+                onClick={() => handleNavigation(path)}
+                className={styles.navButton}
+              >
+                {icon}
+                <span className={styles.navText}>{text}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
 
-          <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
-            {pages.map(({ text, icon, path }) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton onClick={() => router.push(path)}>
-                  <ListItemIcon>{icon}</ListItemIcon>
-                  <ListItemText
-                    primary={text}
-                    primaryTypographyProps={{ fontWeight: "medium" }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            {isUserAuthenticated ? (
-              <>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="User Avatar" />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  {profileActions.map((action) => (
-                    <MenuItem key={action} onClick={handleLogout}>
-                      <Typography textAlign="center">{action}</Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </>
-            ) : (
-              <ListItem disablePadding>
-                <ListItemButton onClick={() => router.push("/authentication")}>
-                  <LoginIcon />
-                  <ListItemText>Authentication</ListItemText>
-                </ListItemButton>
-              </ListItem>
-            )}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+        <div>
+          {isUserAuthenticated ? (
+            <button
+              className={styles.authButton}
+              onClick={() => {
+                handleLogout();
+              }}
+            >
+              <span className="material-symbols-outlined">logout</span>
+              <span className={styles.authText}> Log Out </span>
+            </button>
+          ) : (
+            <button
+              className={styles.authButton}
+              onClick={() => {
+                handleNavigation("/authentication");
+              }}
+            >
+              <span className="material-symbols-outlined">login</span>
+              <span className={styles.authText}> Authenticate</span>
+            </button>
+          )}
+        </div>
+      </nav>
+    </>
   );
 }
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,23 +1,18 @@
 import NavigationMenu from "@/components/navigation-menu";
 import Head from "next/head";
-import { ThemeProvider } from "@mui/material/styles";
-import customTheme from "../themes/custom-theme";
-import CssBaseline from "@mui/material/CssBaseline";
 import AuthenticationProvider from "@/utils/authenticationProvider";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <AuthenticationProvider>
-        <ThemeProvider theme={customTheme}>
-          <CssBaseline />
-          <Head>
-            <title>Citivora</title>
-            <link rel="icon" href="/city-icon.png" />
-          </Head>
-          <NavigationMenu />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <Head>
+          <title>Citivora</title>
+          <link rel="icon" href="/city-icon.png" />
+        </Head>
+        <NavigationMenu />
+        <Component {...pageProps} />
       </AuthenticationProvider>
     </>
   );
