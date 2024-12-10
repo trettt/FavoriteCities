52e9e79362caf21e8c32450390478dc7ba67bc2d
Tret Elena <etret161@gmail.com>
2024-12-10 20:46:10 +0200

    added a slideshow to the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,16 @@
+import Slideshow from "@/components/slideshow";
+import styles from "@/styles/home.module.css";
+
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className={styles.pageContainer}>
+      <div className={styles.slideshowSection}>
+        <Slideshow />
+      </div>
+      <div className={styles.descriptionSection}>
+        <h1 className={styles.title}>Welcome </h1>
+        <p className={styles.description}>Some description</p>
+      </div>
+    </div>
   );
 }
